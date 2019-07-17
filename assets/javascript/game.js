a594a905eaee4be4f68ac2bb791147470849c36c
Tim Sherman <tim.rs.sherman@gmail.com>
2019-07-16 18:00:46 -0700

    guesses decreasing

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,59 @@
-var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var computerChoices = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"
+];
+
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var computerGuess =
+    computerChoices[Math.floor(Math.random() * computerChoices.length)];
 
 document.onkeyup = function (event) {
     var userGuess = event.key;
-    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
+    console.log(userGuess);
+    console.log(computerGuess);
     if (userGuess === computerGuess) {
         wins++;
+        guessesLeft--;
+        console.log("Wins: " + wins);
+        console.log("Guesses-Left: " + guessesLeft);
+        document.getElementById("win-text").innerHTML = "Wins: " + wins;
+        document.getElementById("loss-text").innerHTML = "Losses: " + losses;
+        document.getElementById("guessleft-text").innerHTML =
+            "Guesses Left: " + guessesLeft;
+    } else if (userGuess !== computerGuess) {
+        losses++;
+        guessesLeft--;
+        console.log("Wins: " + wins);
+        console.log("Guesses-Left: " + guessesLeft);
+        document.getElementById("win-text").innerHTML = "Wins: " + wins;
+        document.getElementById("loss-text").innerHTML = "Losses: " + losses;
+        document.getElementById("guessleft-text").innerHTML =
+            "Guesses Left: " + guessesLeft;
     }
-    if (userGuess !== computerGuess) {
-        losses++
-    }
-    for (var i = 9; i === 0; i--) {
-        if (userGuess === computerGuess) {
-            guessesLeft = 9;
-        } else if (userGuess !== computerGuess) {
-            return i;
-        } guessLeft = i;
-    }
-    };
-
-
-    document.getElementById("win-text").innerHTML = "Wins: " + wins;
-    document.getElementById("loss-text").innerHTML = "Losses: " + losses;
-    document.getElementById("guessleft-text").innerHTML = "Guesses Left: " + guessesLeft;
+};
