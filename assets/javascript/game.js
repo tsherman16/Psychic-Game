508ef79b10f607b301ee01764bd4103de28b70e1
Tim Sherman <tim.rs.sherman@gmail.com>
2019-07-16 22:10:53 -0700

    guess so far added

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -26,34 +26,39 @@ var computerChoices = [
     "y",
     "z"
 ];
-
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
+var guessAttempts = [];
 var computerGuess =
     computerChoices[Math.floor(Math.random() * computerChoices.length)];
-
 document.onkeyup = function (event) {
     var userGuess = event.key;
     console.log(userGuess);
     console.log(computerGuess);
     if (userGuess === computerGuess) {
         wins++;
-        guessesLeft--;
+        guessesLeft = 9;
+        guessAttempts = [];
         console.log("Wins: " + wins);
         console.log("Guesses-Left: " + guessesLeft);
+        console.log(guessAttempts);
         document.getElementById("win-text").innerHTML = "Wins: " + wins;
         document.getElementById("loss-text").innerHTML = "Losses: " + losses;
         document.getElementById("guessleft-text").innerHTML =
             "Guesses Left: " + guessesLeft;
+        document.getElementById("guesses-text").innerHTML = guessAttempts;
     } else if (userGuess !== computerGuess) {
         losses++;
         guessesLeft--;
+        guessAttempts.push(userGuess);
         console.log("Wins: " + wins);
         console.log("Guesses-Left: " + guessesLeft);
+        console.log(guessAttempts);
         document.getElementById("win-text").innerHTML = "Wins: " + wins;
         document.getElementById("loss-text").innerHTML = "Losses: " + losses;
         document.getElementById("guessleft-text").innerHTML =
             "Guesses Left: " + guessesLeft;
+        document.getElementById("guesses-text").innerHTML = guessAttempts;
     }
 };
